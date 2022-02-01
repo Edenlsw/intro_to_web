796a0504823dff04b5a33712a7e8fd978b528713
Eden <elsw@hotmail.co.uk>
2022-02-01 12:57:58 +0000

    cat image with border added'

--- sinatra/app.rb
+++ sinatra/app.rb
@@ -3,9 +3,17 @@ require "sinatra/reloader" if development?
 
 
 get '/' do
-  'Newcastle'
+  'Newcastle and brum'
 end
 
 get '/secret' do
   'This is a not a secret'
+end
+
+get '/cat' do
+  "
+  <div style='border: 5px dashed purple'> 
+    <img src = https://i.imgur.com/jFaSxym.png>
+  </div>
+  "
 end
